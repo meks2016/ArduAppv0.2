e086663394b3d4b919cc202e7ccae6bb03e1481b
Stephan Kouch <stephko@outlook.com>
2017-04-09 22:13:40 +0200

    Stephko update

--- app/src/main/java/com/apps/meks/arduapp/MainActivity.java
+++ app/src/main/java/com/apps/meks/arduapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -15,6 +16,15 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.lang.ref.WeakReference;
 import java.util.Set;
@@ -39,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
                     Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
-                    rpmTextView.setText("-");
                     voltageTextView.setText("- V");
                     temperatureTextView.setText("- °C");
                     primerIgnitionTextView.setText("- °C");
@@ -59,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     private static TextView primerIgnitionTextView;
     private static TextView voltageTextView;
     private static TextView temperatureTextView;
-    private static TextView rpmTextView;
+    private static LineChart rpmChart;
 
     private static String inputCommand = "A";
     private static WriteDataThread thread;
@@ -95,19 +104,65 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-
         //UI Elemente
         selectedCurveTextView = (TextView) findViewById(R.id.selectedCurve);
         primerIgnitionTextView = (TextView) findViewById(R.id.primerIgnition);
         voltageTextView = (TextView) findViewById(R.id.voltage);
         temperatureTextView = (TextView) findViewById(R.id.temperature);
-        rpmTextView = (TextView) findViewById(R.id.rpm);
+        rpmChart = (LineChart) findViewById(R.id.chart);
+
+        // enable description text
+        rpmChart.getDescription().setEnabled(false);
+        // if disabled, scaling can be done on x- and y-axis separately
+        rpmChart.setPinchZoom(true);
+        // set an alternative background color
+        rpmChart.setBackgroundColor(getResources().getColor(R.color.colorOliveGreen));
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+        // add empty data
+        rpmChart.setData(data);
+        // get the legend (only possible after setting data)
+        Legend l = rpmChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = rpmChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+
+        YAxis leftAxis = rpmChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(10000);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = rpmChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
 
 
         mHandler = new MyHandler(this);
 
         thread = new WriteDataThread();
         thread.start();
+    }
+
+
+    private static LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Drehzahl");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.WHITE);
+        set.setDrawCircles(false);
+        set.setLineWidth(5);
+        set.setFillColor(Color.WHITE);
+        set.setDrawValues(false);
+        return set;
     }
 
     public class WriteDataThread implements Runnable {
@@ -151,8 +206,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
+        thread.stop();
         unregisterReceiver(mUsbReceiver);
         unbindService(usbConnection);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
         thread.stop();
     }
 
@@ -231,14 +293,36 @@ public class MainActivity extends AppCompatActivity {
                 if(values.length == 5){
                     //split the first valaue - to get the input letter
                     try{
+
                         rpm = Integer.parseInt(values[0]) * 10;
                         voltage = Double.parseDouble(values[1]) / 10;
-                        temperature = Integer.parseInt(values[2].trim());
+                        temperature = Integer.parseInt(values[2]);
                         primerIgnition = Integer.parseInt(values[3]);
-                        selectedCurve = Integer.parseInt(values[4].trim());
+                        selectedCurve = Integer.parseInt(values[4]);
+
+
+                        LineData dataForChart = rpmChart.getData();
+
+                        if (dataForChart != null) {
+
+                            ILineDataSet set = dataForChart.getDataSetByIndex(0);
+
+                            if (set == null) {
+                                set = createSet();
+                                dataForChart.addDataSet(set);
+                            }
+                            dataForChart.addEntry(new Entry(set.getEntryCount(), (float) rpm), 0);
+                            dataForChart.notifyDataChanged();
+                            // let the chart know it's data has changed
+                            rpmChart.notifyDataSetChanged();
+                            // limit the number of visible entries
+                            rpmChart.setVisibleXRangeMaximum(20);
+                            // move to the latest entry
+                            rpmChart.moveViewToX(dataForChart.getEntryCount());
+
+                        }
 
                         //Set UI Elements
-                        rpmTextView.setText(String.valueOf(rpm));
                         voltageTextView.setText(String.valueOf(voltage) + " V");
                         temperatureTextView.setText(String.valueOf(temperature) + " °C");
                         primerIgnitionTextView.setText(String.valueOf(primerIgnition) + " °C");
